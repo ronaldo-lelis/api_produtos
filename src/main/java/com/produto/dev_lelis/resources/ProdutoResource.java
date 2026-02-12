b7e0485b25c4306cd10d7d699afba57f16342c45
Ronaldo Lelis <delelis.locutor@gmail.com>
2026-02-12 17:18:29 -0300

    criando os métodos do CRUD de produtos

--- src/main/java/com/produto/dev_lelis/resources/ProdutoResource.java
+++ src/main/java/com/produto/dev_lelis/resources/ProdutoResource.java
@@ -3,8 +3,12 @@ package com.produto.dev_lelis.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +32,21 @@ public class ProdutoResource {
 		return produtoRepository.findById(id);
 	}
 	
+	@PostMapping("/produto")
+	public Produto salvarProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	@DeleteMapping("/produto")
+	public void deletarProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
+	
+
+	@PutMapping("/produto")
+	public Produto editarProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
 	
 
 }
