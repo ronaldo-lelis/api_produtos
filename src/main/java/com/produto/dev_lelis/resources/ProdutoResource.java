e06a0395262a53ad81cbb1635fc9faddac9e919b
Ronaldo Lelis <delelis.locutor@gmail.com>
2026-02-12 18:20:12 -0300

    criando a documentação usando Swagger

--- src/main/java/com/produto/dev_lelis/resources/ProdutoResource.java
+++ src/main/java/com/produto/dev_lelis/resources/ProdutoResource.java
@@ -3,50 +3,50 @@ package com.produto.dev_lelis.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.produto.dev_lelis.models.Produto;
 import com.produto.dev_lelis.repository.ProdutoRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(value="/api")
+@CrossOrigin(origins="*")
+@Tag(name = "Produtos", description = "Endpoints para gerenciamento de produtos")
 public class ProdutoResource {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
 	@GetMapping("/produtos")
+	@Operation(summary = "Lista todos os produtos")
 	public List<Produto> listarProdutos() {
 		return produtoRepository.findAll();
 	}
 	
 	@GetMapping("/produto/{id}")
+	@Operation(summary = "Busca um produto pelo ID")
 	public Produto listarProdutoUnico(@PathVariable(value="id") long id) {
 		return produtoRepository.findById(id);
 	}
 	
 	@PostMapping("/produto")
+	@Operation(summary = "Cria um novo produto")
 	public Produto salvarProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 
 	@DeleteMapping("/produto")
+	@Operation(summary = "Remove um produto")
 	public void deletarProduto(@RequestBody Produto produto) {
 		produtoRepository.delete(produto);
 	}
 	
-
 	@PutMapping("/produto")
+	@Operation(summary = "Atualiza um produto existente")
 	public Produto editarProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
-	
-
 }
